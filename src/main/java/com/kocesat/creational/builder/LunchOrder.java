ecb85240b8c5d2723b8d442458a6380e5dd62d51
kocesat <kocesat@gmail.com>
2023-05-24 20:11:54 +0300

    builder pattern benchmarking

--- src/main/java/com/kocesat/creational/builder/LunchOrder.java
+++ src/main/java/com/kocesat/creational/builder/LunchOrder.java
@@ -1,6 +1,63 @@
 package com.kocesat.creational.builder;
 
 public class LunchOrder {
+    private String bread;
+    private String condiments;
+    private String dressing;
+    private String meat;
+
+    private LunchOrder(Builder builder) {
+        this.bread = builder.bread;
+        this.condiments = builder.condiments;
+        this.dressing = builder.dressing;
+        this.meat = builder.meat;
+    }
+
+    public LunchOrder(LunchOrderBuilder builder) {
+        this.bread = builder.getBread();
+        this.condiments = builder.getCondiments();
+        this.dressing = builder.getDressing();
+        this.meat = builder.getMeat();
+    }
+
+    // for performance testing
+    public LunchOrder(){}
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public String getBread() {
+        return bread;
+    }
+
+    public String getCondiments() {
+        return condiments;
+    }
+
+    public String getDressing() {
+        return dressing;
+    }
+
+    public String getMeat() {
+        return meat;
+    }
+
+    public void setBread(String bread) {
+        this.bread = bread;
+    }
+
+    public void setCondiments(String condiments) {
+        this.condiments = condiments;
+    }
+
+    public void setDressing(String dressing) {
+        this.dressing = dressing;
+    }
+
+    public void setMeat(String meat) {
+        this.meat = meat;
+    }
 
     public static class Builder {
         private String bread;
@@ -9,7 +66,7 @@ public class LunchOrder {
         private String meat;
 
         // if something required put into the Builder constructor
-        public Builder() {}
+        private Builder() {}
 
         public LunchOrder build() {
             return new LunchOrder(this);
@@ -34,40 +91,5 @@ public class LunchOrder {
             this.meat = meat;
             return this;
         }
-    }
-
-    private final String bread;
-    private final String condiments;
-    private final String dressing;
-    private final String meat;
-
-    private LunchOrder(Builder builder) {
-        this.bread = builder.bread;
-        this.condiments = builder.condiments;
-        this.dressing = builder.dressing;
-        this.meat = builder.meat;
-    }
-
-    public LunchOrder(LunchOrderBuilder builder) {
-        this.bread = builder.getBread();
-        this.condiments = builder.getCondiments();
-        this.dressing = builder.getDressing();
-        this.meat = builder.getMeat();
-    }
-
-    public String getBread() {
-        return bread;
-    }
-
-    public String getCondiments() {
-        return condiments;
-    }
-
-    public String getDressing() {
-        return dressing;
-    }
-
-    public String getMeat() {
-        return meat;
     }
 }
--- src/main/java/com/kocesat/creational/builder/Main.java
+++ src/main/java/com/kocesat/creational/builder/Main.java
@@ -1,32 +1,85 @@
 package com.kocesat.creational.builder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        // First method: static inner class
-        LunchOrder.Builder builder = new LunchOrder.Builder();
-        LunchOrder lunchOrder = builder.bread("Wheat")
-                                        .condiments("Lettuce")
-                                        .dressing("Mayoness")
-                                        .meat("Turkey")
-                                        .build();
+  public static void main(String[] args) {
+    // First method: static inner class (what Lombok uses)
+    LunchOrder lunchOrder = LunchOrder.builder()
+      .bread("Wheat")
+      .condiments("Lettuce")
+      .dressing("Mayoness")
+      .meat("Turkey")
+      .build();
 
-        System.out.println(lunchOrder.getBread());
-        System.out.println(lunchOrder.getCondiments());
-        System.out.println(lunchOrder.getDressing());
-        System.out.println(lunchOrder.getMeat());
+//        System.out.println(lunchOrder.getBread());
+//        System.out.println(lunchOrder.getCondiments());
+//        System.out.println(lunchOrder.getDressing());
+//        System.out.println(lunchOrder.getMeat());
 
-        // Second method: Different builder class
-        LunchOrderBuilder orderBuilder = new LunchOrderBuilder();
-        LunchOrder sandwich = orderBuilder.withBread("Brown")
-                .withCondiments("Lettuce")
-                .withDressing("Ketchup")
-                .withMeat("Kuzu")
-                .build();
+    // Second method: Different builder class
+    LunchOrderBuilder orderBuilder = new LunchOrderBuilder();
+    LunchOrder sandwich = orderBuilder.withBread("Brown")
+      .withCondiments("Lettuce")
+      .withDressing("Ketchup")
+      .withMeat("Kuzu")
+      .build();
 
-        System.out.println("My Sandwich");
-        System.out.println(sandwich.getBread());
-        System.out.println(sandwich.getCondiments());
-        System.out.println(sandwich.getDressing());
-        System.out.println(sandwich.getMeat());
-    }
+//        System.out.println("My Sandwich");
+//        System.out.println(sandwich.getBread());
+//        System.out.println(sandwich.getCondiments());
+//        System.out.println(sandwich.getDressing());
+//        System.out.println(sandwich.getMeat());
+
+    // performance testing of builder pattern
+
+      // warmup
+      int size = 1_000_000;
+      performWithBuilder(size, false);
+      performWithoutBuilder(size, false);
+      // benchmark
+      long withBuilderAvg = 0, withoutBuilderAvg = 0;
+      int iteration = 100;
+      for (int j = 0; j < iteration; j++) {
+          withoutBuilderAvg += performWithoutBuilder(size, true);
+          withBuilderAvg += performWithBuilder(size, true);
+      }
+      System.out.println("average with builder: " + (withBuilderAvg/iteration));
+      System.out.println("average without builder: " + (withoutBuilderAvg/iteration));
+  }
+
+  public static long performWithBuilder(int size, boolean logging) {
+      long start = System.currentTimeMillis();
+      List<LunchOrder> orderListV2 = new ArrayList<>();
+      for (int i = 0; i < size; i++) {
+          orderListV2.add(LunchOrder.builder()
+            .bread("breadV2")
+            .meat("meatV2")
+            .condiments("condimentsV2")
+            .dressing("dressingV2")
+            .build());
+      }
+      long end = System.currentTimeMillis();
+      if (logging)
+          System.out.println("With builder elapsed ms: " + (end - start));
+      return (end - start);
+  }
+
+  public static long performWithoutBuilder(int size, boolean logging) {
+      long start = System.currentTimeMillis();
+      List<LunchOrder> orderListV1 = new ArrayList<>();
+      for (int i = 0; i < size; i++) {
+          var order = new LunchOrder();
+          order.setBread("breadV1");
+          order.setMeat("meatV1");
+          order.setCondiments("condimentsV1");
+          order.setDressing("dressingV1");
+          orderListV1.add(order);
+      }
+      long end = System.currentTimeMillis();
+      if (logging)
+          System.out.println("Without builder elapsed ms: " + (end - start));
+      return (end - start);
+  }
 }
